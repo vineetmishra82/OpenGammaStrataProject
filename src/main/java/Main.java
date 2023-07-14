b2456a08743b8dc75461a6dd252bb8c22788d1e4
Vineet Mishra <vineet_mishra82@hotmail.com>
2023-07-14 18:33:08 +0530

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,15 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
+	
+	static List<String> headers = new ArrayList<String>();
+	static List<Map<String,String>> itemList = new ArrayList<Map<String,String>>();
 
 	public static void main(String[] args) {
 
@@ -18,16 +25,54 @@ public class Main {
 		BufferedReader buffReader = new BufferedReader(fileReader);
 		
 		String line = "";
+		int count = 0;
 		while((line = buffReader.readLine())!=null)
 		{
 			String[] values = line.split(",");
-			
-			for (String string : values) {
-				System.out.print(string+" ");
+		
+			if(count==0)
+			{
+				for (String string : values) {
+					headers.add(string);
+					
+				}
+				count++;
+				
+				System.out.println("headers size is "+headers.size());
 			}
-			System.out.println("\n");
+			
+			else if(values.length>0) {
+				
+				Map<String,String> item = new TreeMap<String, String>();
+				
+				for(int i = 0;i<headers.size();i++)
+				{
+					item.put(headers.get(i), values[i]);
+				}
+				
+				itemList.add(item);
+			}	
+			
+			
+			
 		}
 		
+		if(itemList.size()>0)
+		{
+			for (Map<String,String> item : itemList) {
+				
+				for(Map.Entry<String, String> lineItem : item.entrySet())
+				{
+					System.out.println(lineItem.getKey()+" : "+lineItem.getValue());
+				}
+				
+				System.out.println("\n");	
+			}
+			
+			
+		}
+		
+		buffReader.close();
 		
 	} catch (FileNotFoundException e) {
 		// TODO Auto-generated catch block
